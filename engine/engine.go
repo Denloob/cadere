d44d39f1c506a66eb6b620207a5e3ecc3e5f53a3
Denloob <den@denloob.uk>
2024-02-05 11:28:41 +0200

    style: fmt

--- engine/engine.go
+++ engine/engine.go
@@ -40,76 +40,76 @@ func NewBoard(width, height int) Board {
 }
 
 func (b Board) validateRowIndex(row int) error {
-    if row < 0 || row >= len(b) {
-        return errors.New("row index out of range")
-    }
-    return nil
+	if row < 0 || row >= len(b) {
+		return errors.New("row index out of range")
+	}
+	return nil
 }
 
 func (b Board) validateColIndex(col int) error {
-    if col < 0 || col >= len(b[0]) {
-        return errors.New("col index out of range")
-    }
-    return nil
+	if col < 0 || col >= len(b[0]) {
+		return errors.New("col index out of range")
+	}
+	return nil
 }
 
 func (b Board) Put(row, col int, tile Tile) error {
-    if err := b.validateRowIndex(row); err != nil {
-        return err
-    }
-    if err := b.validateColIndex(col); err != nil {
-        return err
-    }
+	if err := b.validateRowIndex(row); err != nil {
+		return err
+	}
+	if err := b.validateColIndex(col); err != nil {
+		return err
+	}
 
-    if !b[row][col].IsEmpty() {
-        return errors.New("tile already occupied")
-    }
+	if !b[row][col].IsEmpty() {
+		return errors.New("tile already occupied")
+	}
 
-    b[row][col] = tile
-    return nil
+	b[row][col] = tile
+	return nil
 }
 
 func (b Board) ShiftRight(row int) error {
-    if err := b.validateRowIndex(row); err != nil {
-        return err
-    }
+	if err := b.validateRowIndex(row); err != nil {
+		return err
+	}
 
 	rowlen := len(b[row])
 	b[row] = append([]Tile{tileEmpty}, b[row][:rowlen-1]...)
-    return nil
+	return nil
 }
 
 func (b Board) ShiftLeft(row int) error {
-    if err := b.validateRowIndex(row); err != nil {
-        return err
-    }
+	if err := b.validateRowIndex(row); err != nil {
+		return err
+	}
 
 	b[row] = append(b[row][1:], tileEmpty)
-    return nil
+	return nil
 }
 
 func (b Board) ShiftUp(col int) error {
-    if err := b.validateColIndex(col); err != nil {
-        return err
-    }
+	if err := b.validateColIndex(col); err != nil {
+		return err
+	}
 
 	for row := 0; row < len(b)-1; row++ {
 		b[row][col] = b[row+1][col]
 	}
 	b[len(b)-1][col] = tileEmpty
-    return nil
+	return nil
 }
 
 func (b Board) ShiftDown(col int) error {
-    if err := b.validateColIndex(col); err != nil {
-        return err
-    }
+	if err := b.validateColIndex(col); err != nil {
+		return err
+	}
 
 	for row := len(b) - 1; row > 0; row-- {
 		b[row][col] = b[row-1][col]
 	}
 	b[0][col] = tileEmpty
-    return nil
+	return nil
 }
 
 type Game struct {
@@ -137,11 +137,11 @@ func (g *Game) NextPlayer() Player {
 }
 
 func (g Game) PlayerExists(player Player) bool {
-    for _, p := range g.players {
-        if p == player {
-            return true
-        }
-    }
+	for _, p := range g.players {
+		if p == player {
+			return true
+		}
+	}
 
-    return false
+	return false
 }
